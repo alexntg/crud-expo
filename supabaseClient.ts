4c23b8328a3c8bebdbe20e65070d2a3828efe3ce
Alex <157579798+alexntg@users.noreply.github.com>
2024-12-01 21:50:38 +0100

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://cukqazudgbsnphkkmnvz.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1a3FhenVkZ2JzbnBoa2ttbnZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA3MjUwNDEsImV4cCI6MjA0NjMwMTA0MX0.NaQ5MDdc3daC7MAKZFAItTd5FGUkIy1hHI5skAafOdE';
+const SUPABASE_URL = '';
+const SUPABASE_KEY = '';
 
 if (!SUPABASE_URL || !SUPABASE_KEY) {
   throw new Error('Las variables de entorno SUPABASE_URL y SUPABASE_KEY deben estar definidas.');
